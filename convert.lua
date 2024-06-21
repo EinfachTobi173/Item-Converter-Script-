166a710e018a77e7940d7710244e8d2b6e5119fa
EinfachTobi173 <tobiasc1732004@gmail.com>
2024-06-21 22:03:09 +0200

    Update convert.lua

--- convert.lua
+++ convert.lua
@@ -1,56 +1,7 @@
 local items = {
-    yellow_phone = {
-        ["name"] = "yellow_phone",
-        ["label"] = "Yellow Phone",
-        ["weight"] = 150,
-        ["type"] = "item",
-        ["image"] = "yellow_phone.png",
-        ["unique"] = true,
-        ["useable"] = true,
-        ["shouldClose"] = true,
-        ["combinable"] = nil,
-        ["description"] = "They say that Quasar Smartphone is the same as an iPhone, what do you think?"
-    },
-    
-    red_phone = {
-        ["name"] = "red_phone",
-        ["label"] = "Red Phone",
-        ["weight"] = 150,
-        ["type"] = "item",
-        ["image"] = "red_phone.png",
-        ["unique"] = true,
-        ["useable"] = true,
-        ["shouldClose"] = true,
-        ["combinable"] = nil,
-        ["description"] = "They say that Quasar Smartphone is the same as an iPhone, what do you think?"
-    },
-    
-    green_phone = {
-        ["name"] = "green_phone",
-        ["label"] = "Green Phone",
-        ["weight"] = 150,
-        ["type"] = "item",
-        ["image"] = "green_phone.png",
-        ["unique"] = true,
-        ["useable"] = true,
-        ["shouldClose"] = true,
-        ["combinable"] = nil,
-        ["description"] = "They say that Quasar Smartphone is the same as an iPhone, what do you think?"
-    },
-    
-    white_phone = {
-        ["name"] = "white_phone",
-        ["label"] = "White Phone",
-        ["weight"] = 150,
-        ["type"] = "item",
-        ["image"] = "white_phone.png",
-        ["unique"] = true,
-        ["useable"] = true,
-        ["shouldClose"] = true,
-        ["combinable"] = nil,
-        ["description"] = "They say that Quasar Smartphone is the same as an iPhone, what do you think?"
-    }
+    -- Add your items here in the old format
 }
+
 
 for key, item in pairs(items) do
     local convertedItem = string.format(
